33646c5ba1bd1e05edc8dfd55a365029f68db762
saifshamil <126049551+saifshamil@users.noreply.github.com>
2023-10-05 19:59:27 -0700

    calculator
    simple calculator

---
+++ index.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+
+
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter your first number:"
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter your second number:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "select operator",
+        choices: ["Add","Subtract","Multiply","Divide"]
+    }
+]);
+
+let result: number;
+
+switch(answer.operator){
+    case 'Add':
+        result = answer.num1 + answer.num2;
+        console.log("The answer for addition is: " + result);
+        break;
+
+        case 'Subtract':
+            result = answer.num1 - answer.num2;
+            console.log("The answer for subtraction is: " + result);
+            break;
+
+            case 'Multiply':
+                result = answer.num1 * answer.num2;
+                console.log("The answer for multiplication is: " + result);
+                break;
+        
+                case 'Divide':
+                    result = answer.num1 / answer.num2;
+                    console.log("The answer for Division is: " + result);
+                    break;
+
+}
